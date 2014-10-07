c9835d03f7098dd57e8bcce1b06a39bf17387a50
Fredrik Andersson <fredrik@kondensator.se>
2014-10-07 15:32:03 +0200

    verge tiles testing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var lessMiddleware = require('less-middleware');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(lessMiddleware(__dirname + '/public/stylesheets'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
